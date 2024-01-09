e424890ab5435f922c45416afc6bec1ac955df98
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-01-09 16:41:01 +0530

    HOF-Done

---
+++ renderinglist/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HigherOrderComponent from './Component/HigherOrderFunction'
+
+function App() {
+  
+  return (
+    <div>
+      <HigherOrderComponent />
+    </div>
+  )
+}
+
+export default App
